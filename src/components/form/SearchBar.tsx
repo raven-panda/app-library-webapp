181a559882c155a3cf10cdccc2dd492fcd8af587
raven-panda <ambroise.maire7@gmail.com>
2025-02-22 17:39:24 +0100

    edit: edit search bar icon size

--- src/components/form/SearchBar.tsx
+++ src/components/form/SearchBar.tsx
@@ -4,7 +4,7 @@ import Button from './Button';
 export default function SearchBar({ placeholder, name }: { placeholder: string; name: string; }) {
   return <label className="ebr_input" aria-label={placeholder} htmlFor={name}>
     <Button>
-      <Search size={18}/>
+      <Search size={24}/>
     </Button>
     <input placeholder={placeholder} type="text" name={name} id={name} />
   </label>;

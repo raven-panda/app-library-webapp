c617003bd983c0a2629d7ea20d56a726dc250936
raven-panda <ambroise.maire7@gmail.com>
2025-02-22 11:12:19 +0100

    add: created theme provider hook

---
+++ src/hook/theme.tsx
@@ -0,0 +1,47 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from "react";
+
+export type Theme = "dark" | "light";
+
+const ThemeContext = createContext<[Theme, Dispatch<SetStateAction<Theme>>]|undefined>(undefined);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const getInitialTheme = (): Theme => {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark" || savedTheme === "light") return savedTheme;
+  return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+};
+
+export const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [theme, setTheme] = useState<Theme>(document.body.classList.contains("dark") ? "dark" : "light");
+
+  useEffect(() => {
+    const mq = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const themeChangedCallback = (e: MediaQueryListEvent) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
+
+    mq.addEventListener("change", themeChangedCallback);
+    return () => {
+      mq.removeEventListener("change", themeChangedCallback);
+    };
+  }, []);
+
+  useEffect(() => {
+    document.body.classList.toggle("dark", theme === "dark");
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return <ThemeContext.Provider value={[theme, setTheme]}>
+    {children}
+  </ThemeContext.Provider>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useTheme = (): [Theme, Dispatch<SetStateAction<Theme>>] => {
+  const context = useContext(ThemeContext);
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider");
+
+  return context;
+};

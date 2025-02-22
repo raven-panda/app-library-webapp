6ad8a2ab39cd2e5c36c842a50597d552c6fa34f5
raven-panda <ambroise.maire7@gmail.com>
2025-02-22 14:33:35 +0100

    rm: removed suspense in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Suspense, useEffect } from 'react';
-import { useTranslation } from "react-i18next";
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NotFoundPage from './pages/error/not-found-page';
 import { PUBLIC_ROUTES } from './router';
 import { ThemeProvider } from './hook/Theme';
 
 function App() {
-  const {t} = useTranslation();
-
   useEffect(() => {
     document.body.classList.remove("no-transition");
   }, []);
@@ -15,14 +12,12 @@ function App() {
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <Suspense fallback={<div>{t("loading")}...</div>}>
-          <Routes>
-            {PUBLIC_ROUTES.map(({ path, Component }) => (
-              <Route key={path} path={path} element={<Component />} />
-            ))}
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          {PUBLIC_ROUTES.map(({ path, Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );

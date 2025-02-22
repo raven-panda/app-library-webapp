32f4b640ee1a56a701b7b12b0b603a80245c5601
raven-panda <ambroise.maire7@gmail.com>
2025-02-22 16:25:15 +0100

    add: added value process to form component

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,13 +1,23 @@
-import { ReactNode } from "react";
+import { FormEvent, ReactNode } from "react";
 
 export default function EbrForm({ children, className, defaultData, onSubmit }: {children: ReactNode; className?: string; defaultData?: Record<string, any>; onSubmit: (data: any) => void}) {
-  const _onSubmit = (formData: FormData) => {
+  const _onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget as HTMLFormElement);
+
     const dataObject: Record<string, any> = defaultData ?? {};
-    formData.forEach((value, key) => dataObject[key] = value);
+    formData.forEach((value, key) => dataObject[key] = processValue(value));
     onSubmit(dataObject);
   };
+
+  const processValue = (value: any): string|number|boolean|undefined => {
+    if (typeof value === "string")
+      return value.length ? value : undefined;
+
+    return value;
+  };
   
-  return <form action={_onSubmit} className={"ebr_form " + (className ?? "")} noValidate>
+  return <form action="#" onSubmit={_onSubmit} className={"ebr_form " + (className ?? "")} noValidate>
     {children}
   </form>;
 }

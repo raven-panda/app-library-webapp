fe5a2ee00554e8eb1c40b1ea7bf89ed45495db80
raven-panda <ambroise.maire7@gmail.com>
2025-02-22 14:30:51 +0100

    add: created Author and Books interfaces

---
+++ src/types/Book.ts
@@ -0,0 +1,37 @@
+import { IAuthor } from "./Author";
+import { BookFormatType } from "./enums/book/BookFormatEnum";
+import { BookGenreType } from "./enums/book/BookGenreEnum";
+import { BookThemeType } from "./enums/book/BookThemeEnum";
+import { LanguageCode } from "iso-639-1";
+
+export interface IBooks {
+  id?: number;
+  isbin: string;
+  title: string;
+  author: IAuthor;
+  /** @url */
+  coverFileId: string;
+  editor: string;
+  genre: BookGenreType;
+  theme: BookThemeType;
+  otherTheme: string|null;
+  format: BookFormatType;
+  isPhysicalFormat: boolean;
+  languageCode: LanguageCode;
+  reviews: number;
+  averageRate: number;
+  isForRent: boolean;
+  price: number;
+}
+
+export interface IBooksGlobal {
+  id?: number;
+  title: string;
+  authorFullName: string;
+  /** @url */
+  coverFileId: string;
+  reviews: number;
+  averageRate: number;
+  isForRent: boolean;
+  price: number;
+}

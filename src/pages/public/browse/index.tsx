d785942016816cc5ba0d6dd5b9e70691183d1b37
raven-panda <ambroise.maire7@gmail.com>
2025-03-01 12:01:36 +0100

    edit: updated useBrowseBooks hook

--- src/pages/public/browse/index.tsx
+++ src/pages/public/browse/index.tsx
@@ -1,18 +1,12 @@
-import { useSearchParams } from "react-router-dom";
-import { useBrowseBooks } from "../../../hook/api/Book";
-import { useEffect } from "react";
+import {useBrowseBooks} from "../../../hook/api/Book";
+import {useEffect} from "react";
 
 export default function BrowsePage() {
-  const [searchParams] = useSearchParams();
-  const { books, setQuery } = useBrowseBooks();
+  const { books } = useBrowseBooks();
 
   useEffect(() => {
-    setQuery(searchParams);    
-  }, [setQuery, searchParams]);
-
-  useEffect(() => {
-    console.log({books});
+    console.log(books);
   }, [books]);
 
-  return <>Browse</>;
+  return <></>;
 }

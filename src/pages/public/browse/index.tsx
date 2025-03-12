23cf3f593c2a90d12f5ff6748e3b130dea28da59
raven-panda <ambroise.maire7@gmail.com>
2025-03-13 00:15:24 +0100

    add: added message when no books are found

--- src/pages/public/browse/index.tsx
+++ src/pages/public/browse/index.tsx
@@ -1,5 +1,7 @@
 import {useBrowseBooks} from "@/hook/api/Book.ts";
 import {useEffect} from "react";
+import CollectionUtils from "@/lib/utils/CollectionUtils.ts";
+import {Link} from "react-router-dom";
 
 export default function BrowsePage() {
   const { books } = useBrowseBooks();
@@ -8,5 +10,7 @@ export default function BrowsePage() {
     console.log({books});
   }, [books]);
 
+  if (CollectionUtils.isEmpty(books))
+    return <>Aucun résultat. Essayer de chercher autre chose. <Link to={""} className="link">Effacer la recherche</Link></>;
   return <></>;
 }

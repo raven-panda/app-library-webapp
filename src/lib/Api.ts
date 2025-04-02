b5b3503db6c1518636c503aa1e9cd5d85cb35b88
raven-panda <ambroise.maire7@gmail.com>
2025-04-02 19:05:49 +0200

    add: added fixture files url to API url constant

--- src/lib/Api.ts
+++ src/lib/Api.ts
@@ -1,8 +1,11 @@
-const { VITE_API_BASE_URL } = import.meta.env;
+const { VITE_API_BASE_URL, VITE_WEBAPP_BASE_URL } = import.meta.env;
+
+const isFixturesEnabled = import.meta.env.VITE_ENABLE_FIXTURES === 'true';
 
 export const API_BOOK = `${VITE_API_BASE_URL}/book`;
 export const API_BOOK_BY_ID = `${VITE_API_BASE_URL}/book/:id`;
-export const API_BOOK_GLOBAL = `${VITE_API_BASE_URL}/book/global`;
 export const API_BOOK_SEARCH = `${VITE_API_BASE_URL}/book/search`;
 
-export const API_FILE = `${VITE_API_BASE_URL}/file/:id`;
+export const API_FILE = isFixturesEnabled
+  ? `${VITE_WEBAPP_BASE_URL}/_fixtures/file/book/:id`
+  : `${VITE_API_BASE_URL}/file/:id`;

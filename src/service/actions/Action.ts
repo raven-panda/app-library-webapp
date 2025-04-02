1209644ab9cf0129c65f7fa15608da033bba414e
raven-panda <ambroise.maire7@gmail.com>
2025-04-02 19:00:56 +0200

    add: added mocked book service that uses fixtures

--- src/service/actions/Action.ts
+++ src/service/actions/Action.ts
@@ -2,16 +2,6 @@ import axios, { AxiosError } from 'axios';
 import { ApiResponse } from '@/lib/types/ApiResponse.ts';
 import UrlTransformer from '@/service/UrlTransformer.ts';
 
-const ApiCall_Mock = () => {
-  throw new Error('Not implemented');
-};
-
-const ApiCallWithToken_Mock = () => {
-  throw new Error('Not implemented');
-};
-
-const isFixturesEnabled = import.meta.env.VITE_ENABLE_FIXTURES === 'true';
-
 export type ApiCallFnType = () => {
   get<TData>(
     url: string,
@@ -34,93 +24,89 @@ export type ApiCallWithTokenFnType = (jwt: string) => {
   ): Promise<ApiResponse<TData>>;
 };
 
-export const ApiCall: ApiCallFnType = isFixturesEnabled
-  ? ApiCall_Mock
-  : () => ({
-      get: async <TData>(
-        url: string,
-        params?: Record<string, any>,
-      ): Promise<ApiResponse<TData>> => {
-        let finalUrl = url;
-        if (params) {
-          finalUrl += '?' + UrlTransformer.encodeFromObject(params);
-        }
+export const ApiCall: ApiCallFnType = () => ({
+  get: async <TData>(
+    url: string,
+    params?: Record<string, any>,
+  ): Promise<ApiResponse<TData>> => {
+    let finalUrl = url;
+    if (params) {
+      finalUrl += '?' + UrlTransformer.encodeFromObject(params);
+    }
 
-        try {
-          const { data, status } = await axios.get(finalUrl);
-          return { data, status };
-        } catch (error) {
-          console.error('Error getting data :', (error as any).message);
-          return {
-            error: (error as any).message,
-            status: (error as AxiosError).status,
-          };
-        }
-      },
-      post: async <TPayload, TData>(
-        url: string,
-        payload: TPayload,
-      ): Promise<ApiResponse<TData>> => {
-        try {
-          const { data, status } = await axios.post(url, payload);
-          return { data, status };
-        } catch (error) {
-          console.error('Error posting data :', (error as any).message);
-          return {
-            error: (error as any).message,
-            status: (error as any).status,
-          };
-        }
-      },
-    });
+    try {
+      const { data, status } = await axios.get(finalUrl);
+      return { data, status };
+    } catch (error) {
+      console.error('Error getting data :', (error as any).message);
+      return {
+        error: (error as any).message,
+        status: (error as AxiosError).status,
+      };
+    }
+  },
+  post: async <TPayload, TData>(
+    url: string,
+    payload: TPayload,
+  ): Promise<ApiResponse<TData>> => {
+    try {
+      const { data, status } = await axios.post(url, payload);
+      return { data, status };
+    } catch (error) {
+      console.error('Error posting data :', (error as any).message);
+      return {
+        error: (error as any).message,
+        status: (error as any).status,
+      };
+    }
+  },
+});
 
 /** @todo: implement jwt auth when needed */
-export const ApiCallWithToken: ApiCallWithTokenFnType = isFixturesEnabled
-  ? ApiCallWithToken_Mock
-  : (jwt: string) => {
-      const headers = {
-        Authorization: `Bearer ${jwt}`,
-      };
+export const ApiCallWithToken: ApiCallWithTokenFnType = (jwt: string) => {
+  const headers = {
+    Authorization: `Bearer ${jwt}`,
+  };
 
-      return {
-        get: async <TData>(
-          url: string,
-          params?: Record<string, any>,
-        ): Promise<ApiResponse<TData>> => {
-          let finalUrl = url;
-          if (params) {
-            finalUrl += '?' + UrlTransformer.encodeFromObject(params);
-          }
+  return {
+    get: async <TData>(
+      url: string,
+      params?: Record<string, any>,
+    ): Promise<ApiResponse<TData>> => {
+      let finalUrl = url;
+      if (params) {
+        finalUrl += '?' + UrlTransformer.encodeFromObject(params);
+      }
 
-          try {
-            const { data, status } = await axios.get(finalUrl, {
-              headers,
-            });
-            return { data, status };
-          } catch (error) {
-            console.error('Error getting data :', (error as any).message);
-            return {
-              error: (error as any).message,
-              status: (error as any).status,
-            };
-          }
-        },
-        post: async <TPayload, TData>(
-          url: string,
-          payload: TPayload,
-        ): Promise<ApiResponse<TData>> => {
-          try {
-            const { data, status } = await axios.post(url, payload, {
-              headers,
-            });
-            return { data, status };
-          } catch (error) {
-            console.error('Error posting data :', (error as any).message);
-            return {
-              error: (error as any).message,
-              status: (error as any).status,
-            };
-          }
-        },
-      };
-    };
+      try {
+        const { data, status } = await axios.get(finalUrl, {
+          headers,
+        });
+        return { data, status };
+      } catch (error) {
+        console.error('Error getting data :', (error as any).message);
+        return {
+          error: (error as any).message,
+          status: (error as any).status,
+        };
+      }
+    },
+    post: async <TPayload, TData>(
+      url: string,
+      payload: TPayload,
+    ): Promise<ApiResponse<TData>> => {
+      try {
+        const { data, status } = await axios.post(url, payload, {
+          headers,
+        });
+        return { data, status };
+      } catch (error) {
+        console.error('Error posting data :', (error as any).message);
+        return {
+          error: (error as any).message,
+          status: (error as any).status,
+        };
+      }
+    },
+  };
+};
--- src/service/actions/BooksService.ts
+++ src/service/actions/BooksService.ts
@@ -4,8 +4,17 @@ import { URLSearchParams } from 'node:url';
 import { ApiCall } from '@/service/actions/Action.ts';
 import { API_BOOK_BY_ID, API_BOOK_SEARCH } from '@/lib/Api.ts';
 import { ApiResponse } from '@/lib/types/ApiResponse.ts';
+import books_fixture from '@/_fixtures/books/all.ts';
+import { ISearchBooks } from '@/lib/types/dto/Books.ts';
 
-class BooksService {
+const isFixturesEnabled = import.meta.env.VITE_ENABLE_FIXTURES === 'true';
+
+interface IBookService {
+  getById(id: string): Promise<ApiResponse<IBook>>;
+  search(queryParams: URLSearchParams): Promise<ApiResponse<IBookGlobal[]>>;
+}
+
+class BooksService implements IBookService {
   async getById(id: string): Promise<ApiResponse<IBook>> {
     return ApiCall().get(API_BOOK_BY_ID.replace(':id', id));
   }
@@ -19,5 +28,94 @@ class BooksService {
   }
 }
 
-const booksService = new BooksService();
+class BooksService_Mock implements IBookService {
+  private processSearchAll(searchString: string, book: IBook): boolean {
+    return (
+      (
+        book.author.firstName.toLowerCase() +
+        ' ' +
+        book.author.lastName.toLowerCase()
+      ).includes(searchString) ||
+      book.title.toLowerCase().includes(searchString) ||
+      book.editor.toLowerCase().includes(searchString) ||
+      book.isbn.toLowerCase().includes(searchString)
+    );
+  }
+
+  async getById(id: string): Promise<ApiResponse<IBook>> {
+    const foundBook = books_fixture.find((book) => book.id === id);
+
+    return {
+      data: foundBook,
+      status: foundBook ? 200 : 404,
+    };
+  }
+
+  async search(
+    queryParams: URLSearchParams,
+  ): Promise<ApiResponse<IBookGlobal[]>> {
+    const searchQuery: ISearchBooks =
+      UrlTransformer.transformSearchBooksQuery(queryParams);
+
+    return {
+      data: books_fixture
+        .filter(
+          (book) =>
+            (!searchQuery.searchAll ||
+              this.processSearchAll(
+                searchQuery.searchAll.toLowerCase(),
+                book,
+              )) &&
+            (!searchQuery.author ||
+              (
+                book.author.firstName.toLowerCase() +
+                ' ' +
+                book.author.lastName.toLowerCase()
+              ).includes(searchQuery.author.toLowerCase())) &&
+            (!searchQuery.title ||
+              book.title
+                .toLowerCase()
+                .includes(searchQuery.title.toLowerCase())) &&
+            (!searchQuery.editor ||
+              book.editor
+                .toLowerCase()
+                .includes(searchQuery.editor.toLowerCase())) &&
+            (!searchQuery.isbn ||
+              book.isbn
+                .toLowerCase()
+                .includes(searchQuery.isbn.toLowerCase())) &&
+            (!searchQuery.genre || book.genre.includes(searchQuery.genre)) &&
+            (!searchQuery.themes ||
+              book.themes.some((theme) =>
+                searchQuery.themes?.includes(theme),
+              )) &&
+            (!searchQuery.targetAudience ||
+              book.targetAudience.includes(searchQuery.targetAudience)) &&
+            (!searchQuery.language ||
+              book.languageCode.includes(searchQuery.language.toLowerCase())) &&
+            (!searchQuery.minReviewsNumber ||
+              book.reviews >= searchQuery.minReviewsNumber) &&
+            (!searchQuery.priceRange ||
+              (book.price >= searchQuery.priceRange[0] &&
+                book.price < searchQuery.priceRange[1])),
+        )
+        .map((book) => ({
+          id: book.id ?? '',
+          authorFullName: book.author.firstName + ' ' + book.author.lastName,
+          reviews: book.reviews,
+          editor: book.editor,
+          title: book.title,
+          price: book.price,
+          isForRent: book.isForRent,
+          coverFileId: book.coverFileId,
+          averageRate: book.averageRate,
+        })),
+      status: 200,
+    };
+  }
+}
+
+const booksService = isFixturesEnabled
+  ? new BooksService_Mock()
+  : new BooksService();
 export default booksService;
